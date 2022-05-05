cd71a3bbfb4ddbcd67259fbcc1470fac4d7f1030
wullewutz <wullewutz@users.noreply.github.com>
2022-05-05 17:35:24 +0200

    Basic clap CLI options an upgrade to egui 0.18

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,121 @@
-mod source;
-
-use source::Channels;
+use std::collections::VecDeque;
 use std::io::BufRead;
-use std::sync::{Arc, Mutex};
+use std::sync::mpsc::{channel, Receiver};
 use std::thread;
 
-use eframe::{epi, NativeOptions};
+use eframe::NativeOptions;
 use egui::{
-    plot::{Line, Plot},
+    plot::{Line, Plot, Value, Values},
     Context,
 };
 
-struct App {
-    chans: Arc<Mutex<Channels>>,
+use clap::{crate_version, Arg, Command};
+
+struct ZackApp {
+    pub channels: Vec<VecDeque<Value>>,
+    receiver: Receiver<Vec<f64>>,
+    buffer_length: Box<usize>,
+    x: f64,
 }
 
-impl App {
-    fn new() -> Self {
+impl ZackApp {
+    fn new(
+        cc: &eframe::CreationContext<'_>,
+        receiver: Receiver<Vec<f64>>,
+        buffer_length: Box<usize>,
+    ) -> Self {
+        cc.egui_ctx.set_visuals(egui::Visuals::dark());
         Self {
-            chans: Arc::new(Mutex::new(Channels::new())),
+            channels: vec![],
+            receiver,
+            x: 0.0,
+            buffer_length,
         }
     }
 }
 
-impl epi::App for App {
-    fn update(&mut self, ctx: &Context, _: &eframe::epi::Frame) {
+impl eframe::App for ZackApp {
+    fn update(&mut self, ctx: &Context, _: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             let plot = Plot::new("measurements");
             plot.show(ui, |plot_ui| {
-                for values in self.chans.lock().unwrap().get_channels() {
-                    plot_ui.line(Line::new(values));
+                for chunks in self.receiver.try_iter() {
+                    self.x += 1.0;
+                    while chunks.len() > self.channels.len() {
+                        self.channels.push(VecDeque::default());
+                        println!("Added channel nr. {}", self.channels.len());
+                    }
+                    for (i, ch) in self.channels.iter_mut().enumerate() {
+                        if ch.len() > *self.buffer_length {
+                            ch.pop_front();
+                        }
+
+                        ch.push_back(Value {
+                            x: self.x,
+                            y: chunks[i],
+                        });
+                    }
+                }
+                for ch in &self.channels {
+                    plot_ui.line(Line::new(Values::from_values_iter(ch.iter().copied())));
                 }
             });
         });
         ctx.request_repaint();
     }
-
-    fn name(&self) -> &str {
-        "zack"
-    }
 }
 
 fn main() {
-    let app = App::new();
+    let matches = Command::new("Zack - plott CSV (-ish) streams in realtime")
+        .about("Plott CSV (-ish) streams in realtime")
+        .version(crate_version!())
+        .arg(
+            Arg::new("buf_length")
+                .help(
+                    "How many point of each channels should \
+                     be displayed before dropping the oldest",
+                )
+                .short('b')
+                .long("buffer")
+                .default_value("10000"),
+        )
+        .get_matches();
 
-    let chans = app.chans.clone();
+    let buf_length = Box::new(
+        matches
+            .value_of("buf_length")
+            .expect("Invalid buffer length provided")
+            .parse::<usize>()
+            .expect("Buffer length needs to be a positiv integer"),
+    );
+
+    let (sender, receiver) = channel();
 
     thread::spawn(move || {
         let stdin = std::io::stdin();
         for line in stdin.lock().lines() {
             match line {
-                Ok(s) => chans.lock().unwrap().parse_line(&s),
+                Ok(s) => {
+                    // trim unwanted leading and trailing characters and split at one
+                    // of the possible seperators
+                    let sep = |c| c == ',' || c == ';' || c == ' ' || c == '\t';
+                    let s = s.trim_start_matches(sep);
+                    let s = s.trim_end_matches(sep);
+                    let str_chunks = s.split(sep).collect::<Vec<&str>>();
+                    let chunks = str_chunks
+                        .iter()
+                        .map(|y| y.parse::<f64>().unwrap_or(0.0))
+                        .collect();
+                    sender.send(chunks).unwrap();
+                }
                 _ => return,
             }
         }
     });
 
-    let native_options = NativeOptions::default();
-    eframe::run_native(Box::new(app), native_options);
+    eframe::run_native(
+        "zack",
+        NativeOptions::default(),
+        Box::new(|cc| Box::new(ZackApp::new(cc, receiver, buf_length))),
+    );
 }
